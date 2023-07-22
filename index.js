e9c221dff1eb28078ff3e4a8b66cb0da79624844
Kristján Oddsson <koddsson@gmail.com>
2023-07-22 21:38:36 +0200

    show debug info if flag is set

--- index.js
+++ index.js
@@ -3,10 +3,18 @@
 import fs from "node:fs/promises";
 import { cwd } from "node:process";
 import path from "node:path";
-import { exit } from "node:process";
+import { env, exit } from "node:process";
 import assert from "node:assert";
 
 import sortPackageJson from "sort-package-json";
+
+const DEBUG = !!env.DEBUG
+
+if (DEBUG) {
+  console.log(cwd());
+  console.log(path.join(cwd(), "package.json"));
+  console.log('---');
+}
 
 const content = await fs.readFile(path.join(cwd(), "package.json"));
 const json = JSON.parse(content);
